45a70bcbbfb6a67f1c62f2bfb42f45ff955088c6
baki003 <2403108500@qq.com>
2026-05-21 14:59:27 +0800

    支持项目描述换行

--- config/user-data/projects.ts
+++ config/user-data/projects.ts
@@ -14,7 +14,7 @@ export const UserProjectsData: Project[] = [
       "业务方面：覆盖关键词采集，整店采集，详情采集等电商业务中常见的采集需求。\n\n" +
       "技术方面：模拟的采集使用 drissionpage，协议/接口获取数据的细节以及 js 逆向不对外透露，卡密后端使用 django，电脑机器码+卡密进行校验。\n\n" +
       "想了解更多技术细节或者有合作意向以及定制需求可以联系我，我会详细解答。",
-    url: "https://github.com/baki003/QN_CService.git",
+    url: "https://github.com/baki003/CJ_open.git",
     previewImage: "/images/caiji.png",
   },
   {
@@ -25,7 +25,7 @@ export const UserProjectsData: Project[] = [
       "我要特别介绍它的亮点：无风险首发所有类型的消息（语音，视频，图片），得益于当前攻克的逆向算法，无视千牛版本，无任何风控问题，本质上就是千牛客户端本身的收发消息逻辑。\n\n" +
       "技术方面：逆向技术不过多透露，ai 方面使用自己进行微调的自研 ai 模型，也支持自己购买 token 接入其它大模型进行回复。\n\n" +
       "想了解更多技术细节或者有合作意向以及定制需求可以联系我，我会详细解答。",
-    url: "https://github.com/baki003/CJ_open.git",
+    url: "https://github.com/baki003/QN_CService.git",
     previewImage: "/images/qianniukefu.png",
   },
 ]
