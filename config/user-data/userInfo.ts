b86fc5f3d32a18a9d9b44ce94edd3a265a52ac75
baki003 <2403108500@qq.com>
2026-05-21 11:37:55 +0800

    更新作品集内容

--- config/user-data/userInfo.ts
+++ config/user-data/userInfo.ts
@@ -1,10 +1,10 @@
 export const UserInfoData = {
-  fullName: "你的名字",
+  fullName: "baki0003",
   tagLine: "开发者 · 个人项目",
   location: "中国",
-  miniBio: "这里写一两句自我介绍，例如主要技术方向或你在做什么。",
+  miniBio: "我只想让自己获得幸福，仅此而已，我要靠现实中属于自己的力量来获得幸福",
   heroImage: {
     type: "url",
-    url: "/images/jc_img_1.png",
+    url: "/images/asuka.png",
   },
 }
